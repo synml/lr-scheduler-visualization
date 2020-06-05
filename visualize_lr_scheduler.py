8922b29abc0e3e6c0c2d3a207b18443d70603768
Lulin <kistssy+dev@gmail.com>
2020-06-05 11:07:16 +0900

    Apply editable number of epoch

--- visualize_lr_scheduler.py
+++ visualize_lr_scheduler.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 
 class Model(torch.nn.Module):
-
     def __init__(self):
         super(Model, self).__init__()
         self.linear = torch.nn.Linear(3, 1)
@@ -19,17 +18,18 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=9, gamma=0.8)
 scheduler_name = scheduler.__class__.__name__
 
+num_epoch = 100
 lr_list = []
-for epoch in range(100):
+for epoch in range(num_epoch):
     lr = None
     for param_group in optimizer.param_groups:
         lr = param_group['lr']
-    print('epoch: {:3d}, lr={:.8f}'.format(epoch, lr))
+    print('epoch: {:3d},   lr: {:.8f}'.format(epoch, lr))
     lr_list.append(lr)
     scheduler.step()
 
 plt.title(scheduler_name)
-plt.xticks(range(0, 100, scheduler.step_size))
+plt.xticks(range(0, num_epoch, scheduler.step_size))
 plt.ylim(0, optimizer.defaults['lr'] + optimizer.defaults['lr'] / 10)
 plt.plot(lr_list)
 plt.show()
